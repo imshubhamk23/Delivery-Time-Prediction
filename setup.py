c8717425a73816607e899b1ef58ff8b0b4df542f
Shubham Kolekar <kolekar.shubham95@gmail.com>
2023-04-27 17:39:33 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace('/n','') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+
+
+
+setup(
+    name='Delivery Time Prediction',
+    version='0.0.1',
+    author='Shubham Kolekar',
+    author_email='kolekar.shubham95@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt')
+)
